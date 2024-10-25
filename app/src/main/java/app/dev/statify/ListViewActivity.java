9547919506c3a01643c70b30c902924731466d42
Rey <saskia.rey96@gmail.com>
2024-10-25 14:12:33 +0200

    Added Buttons to create new Data or Edit existing

--- app/src/main/java/app/dev/statify/ListViewActivity.java
+++ app/src/main/java/app/dev/statify/ListViewActivity.java
@@ -2,9 +2,13 @@ package app.dev.statify;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,12 +20,13 @@ import java.util.Arrays;
 public class ListViewActivity extends AppCompatActivity {
 
     private EditText mEditText;
-   // private Button mButton;
+    private Button mBtnNewData, mBtnEditData;
     private ListView mListView;
     private ArrayList<ArrayList<Double>> mArrayList;
     private ArrayAdapter<ArrayList<Double>> mAdapter;
     SharedPreferences mStatRowSettings;
     private final String PREFS_NAME = "StatRowPrefs";
+    private boolean isEditMode = false;
 
     private void handleSubmit() {
         String s = mEditText.getText().toString().trim();
@@ -61,7 +66,7 @@ public class ListViewActivity extends AppCompatActivity {
     }
 
     // Convert Insertion into Gson for saving statistical row
-    private void saveStatRows(){
+    private void saveStatRows() {
         SharedPreferences.Editor editor = mStatRowSettings.edit();
         Gson gson = new Gson();
         String json = gson.toJson(mArrayList);
@@ -70,10 +75,10 @@ public class ListViewActivity extends AppCompatActivity {
     }
 
     // Reconvert Gson to ArrayList Element for loading last statistical rows
-    private void loadStatRows(){
+    private void loadStatRows() {
         Gson gson = new Gson();
         String json = mStatRowSettings.getString("arrayList_data", null);
-        if(json != null) {
+        if (json != null) {
             java.lang.reflect.Type type = new com.google.gson.reflect.TypeToken<ArrayList<ArrayList<Double>>>() {
             }.getType();
             mArrayList = gson.fromJson(json, type);
@@ -85,28 +90,90 @@ public class ListViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_listview);
 
-        //Initialize instance Variables
+        initializeViews();
+        setUpListeners();
+        loadStatRows();
+        setUpAdapters();
+    }
+
+    private void initializeViews() {
         mEditText = findViewById(R.id.idEditText);
         mListView = findViewById(R.id.idListView);
+        mBtnNewData = findViewById(R.id.idBtnNewData);
+        mBtnEditData = findViewById(R.id.idBtnEditData);
         mStatRowSettings = getSharedPreferences(PREFS_NAME, 0);
+    }
 
-        loadStatRows();
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mArrayList);
-        mListView.setAdapter(mAdapter);
+    private void setUpListeners() {
 
-        mListView.setOnItemClickListener((parent, view, position, id) -> handleItemClick(position));
+        mBtnNewData.setOnClickListener(v -> changeTextVisibility(true));
+        mBtnEditData.setOnClickListener(v -> changeEditMode());
 
         mEditText.setOnKeyListener((v, keyCode, keyEvent) -> {
-            if((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+            if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                 handleSubmit();
                 return true;
             }
             return false;
         });
+
+        mListView.setOnItemClickListener((parent, view, position, id) -> {
+            if (isEditMode) {
+                editItem(position);
+            } else {
+                handleItemClick(position);
+            }
+        });
+    }
+
+    private void setUpAdapters() {
+        mAdapter = new ArrayAdapter<ArrayList<Double>>(this, android.R.layout.simple_list_item_1, mArrayList) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                if (isEditMode) {
+                    view.setBackgroundColor(Color.LTGRAY);
+                } else {
+                    view.setBackgroundColor(Color.TRANSPARENT);
+                }
+                return view;
+            }
+        };
+        mListView.setAdapter(mAdapter);
+    }
+
+
+    private void changeTextVisibility(boolean visible) {
+        mEditText.setVisibility(visible ? View.VISIBLE : View.GONE);
+        if (visible) {
+            mEditText.requestFocus();
+        }
+    }
+
+    private void changeEditMode() {
+        isEditMode = !isEditMode;
+        mBtnEditData.setText(isEditMode ? "Fertig" : "Datensatz bearbeiten");
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void editItem(int position) {
+        ArrayList<Double> item = mArrayList.get(position);
+        StringBuilder sb = new StringBuilder();
+        for (Double d : item) {
+            sb.append(d).append(" ");
+        }
+        mEditText.setText(sb.toString().trim());
+        mEditText.setVisibility(View.VISIBLE);
+        mEditText.requestFocus();
+        mArrayList.remove(position);
+        mAdapter.notifyDataSetChanged();
+
     }
 }
